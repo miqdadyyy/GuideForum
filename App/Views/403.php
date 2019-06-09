c8764661516628c802b3a68e212684830ddc1d49
Verdy Bangkit <verdybangkit@gmail.com>
2019-06-09 23:25:49 +0700

    404 403

---
+++ App/Views/403.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>404 NOT FOUND</title>
+  <link rel="stylesheet" href="vendors/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="bg-shape">
+  <section class="hero-banner magic-ball">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <h1 style="font-size: 18vw; text-align: center;">403</h1>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <h2 style="font-size: 4.5vw; text-align: center;">Forbidden</h2>
+          <p style="font-size: 1.5vw; text-align: center;">Sorry, but you don't have permission to access this page. You can go back to <a href="#">previous page</a></p>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+</body>
+</html>
---
+++ App/Views/404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>404 NOT FOUND</title>
+  <link rel="stylesheet" href="vendors/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="bg-shape">
+  <section class="hero-banner magic-ball">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <h1 style="font-size: 18vw; text-align: center;">404</h1>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <h2 style="font-size: 4.5vw; text-align: center;">Oops! Nothing was found</h2>
+          <p style="font-size: 1.5vw; text-align: center;">The page you are looking for might have been removed had its name changed or is temporarily unavailable. <a href="#">Return to homepage</a></p>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+</body>
+</html>
