81b918b791d3af8fcc219df8a3c34cbbfad1ee70
Verdy Bangkit <verdybangkit@gmail.com>
2019-06-09 20:28:57 +0700

    Frontend done

---
+++ App/Views/user/content-forum.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Content Forum</title>
+  <link rel="stylesheet" href="../vendors/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="../vendors/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="../vendors/themify-icons/themify-icons.css">
+  <link rel="stylesheet" href="../vendors/linericon/style.css">
+  <link rel="stylesheet" href="../vendors/owl-carousel/owl.theme.default.min.css">
+  <link rel="stylesheet" href="../vendors/owl-carousel/owl.carousel.min.css">
+  <link rel="stylesheet" href="../css/magnific-popup.css">
+  <link rel="stylesheet" href="../vendors/flat-icon/font/flaticon.css">
+  <link rel="stylesheet" href="../vendors/nice-select/nice-select.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+  <!--================ Header Menu Area start =================-->
+  <?php
+  include_once "header.php";
+  ?>
+  <!--================Header Menu Area =================-->
+
+  <section class="blog_area single-post-area section-margin">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 posts-list">
+          <div class="single-post">
+            <div class="feature-img justify-content-center">
+              <img class="img-fluid" src="../img/blog/main-blog/m-blog-1.jpg" alt="">
+            </div>
+            <div class="blog_details">
+              <h2>Second divided from form fish beast made every of seas
+                all gathered us saying he our</h2>
+                <p class="excert">
+                  MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower
+
+                </p>
+                <p>
+                  MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually
+                </p>
+                <div class="quote-wrapper">
+                  <div class="quotes">
+                    MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training.
+                  </div>
+                </div>
+
+
+                <p>
+                  MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower
+
+                </p>
+                <p>
+                  MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually
+                </p>
+              </div>
+              <div class="d-none d-sm-block mb-5 pb-4">
+                <div id="map" style="height: 480px;"></div>
+                <script>
+                  function initMap() {
+                    var uluru = {lat: -25.363, lng: 131.044};
+                    var grayStyles = [
+                      {
+                        featureType: "all",
+                        stylers: [
+                          { saturation: -90 },
+                          { lightness: 50 }
+                        ]
+                      },
+                      {elementType: 'labels.text.fill', stylers: [{color: '#A3A3A3'}]}
+                    ];
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                      center: {lat: -31.197, lng: 150.744},
+                      zoom: 9,
+                      styles: grayStyles,
+                      scrollwheel:  false
+                    });
+                  }
+
+                </script>
+                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpfS1oRGreGSBU5HHjMmQ3o5NLw7VdJ6I&callback=initMap"></script>
+
+              </div>
+              <ul class="blog-info-link mt-3 mb-4">
+                <li><a href="#" class="badge badge-light">Lifestyle</a></li>
+                <li><a href="#" class="badge badge-light">Sport</a></li>
+              </ul>
+
+            </div>
+            <div class="navigation-top">
+              <div class="d-sm-flex justify-content-between text-center">
+                <p class="like-info"><span class="align-middle"><i class="far fa-heart"></i></span> Lily and 4 people like this</p>
+                <ul class="social-icons">
+                  <li><p class="comment-count"><span class="align-middle"><i class="far fa-comment"></i></span>06</p></li>
+                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                  <li><a href="#"><i class="fab fa-behance"></i></a></li>
+                </ul>
+              </div>
+
+            <div class="blog-author">
+              <div class="media align-items-center">
+                <img src="../img/blog/author.png" alt="">
+                <div class="media-body">
+                  <a href="#">
+                    <h4><b>Harvard milan</b></h4>
+                  </a>
+                  <p>Second divided from form fish beast made. Every of seas all gathered use saying you're, he our dominion twon Second divided from</p>
+                </div>
+              </div>
+            </div>
+            <div class="comment-form">
+              <h4>Response</h4>
+              <form class="form-contact comment_form" action="#" id="commentForm">
+                <div class="row">
+                  <div class="col-12">
+                    <div class="form-group">
+                      <textarea class="form-control w-100" name="comment" id="comment" cols="30" rows="9" placeholder="Write Comment"></textarea>
+                    </div>
+                    <div class="form-group">
+                      <button type="submit" class="button button-contactForm">Send Message</button>
+                    </div>
+                  </div>
+                </form>
+            </div>
+            <div class="comments-area">
+              <h4>05 Response</h4>
+              <div class="comment-list bg-gray" style="margin-bottom: 48px; padding: 24px 32px; border-radius: 16px;">
+                <div class="single-comment justify-content-between d-flex">
+                  <div class="user justify-content-between d-flex">
+                    <div class="thumb">
+                      <img src="../img/blog/c1.png" alt="">
+                    </div>
+                    <div class="d-flex justify-content-between">
+                      <div class="d-flex align-items-center">
+                        <h5>
+                          <a href="#">Emilly Blunt</a>
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="desc" style="margin-top: 12px;">
+                  <p class="comment">
+                    Multiply sea night grass fourth day sea lesser rule open subdue female fill which them Blessed, give fill lesser bearing multiply sea night grass fourth day sea lesser
+                  </p>
+                </div>
+              </div>
+              <div class="comment-list bg-gray" style="margin-bottom: 48px; padding: 24px 32px; border-radius: 16px;">
+                <div class="single-comment justify-content-between d-flex">
+                  <div class="user justify-content-between d-flex">
+                    <div class="thumb">
+                      <img src="../img/blog/c1.png" alt="">
+                    </div>
+                    <div class="d-flex justify-content-between">
+                      <div class="d-flex align-items-center">
+                        <h5>
+                          <a href="#">Emilly Blunt</a>
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="desc" style="margin-top: 12px;">
+                  <p class="comment">
+                    Multiply sea night grass fourth day sea lesser rule open subdue female fill which them Blessed, give fill lesser bearing multiply sea night grass fourth day sea lesser
+                  </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <!-- ================ start footer Area ================= -->
+    <?php
+    include_once "footer.php";
+    ?>
+    <!-- ================ End footer Area ================= -->
+
+    <script src="../vendors/jquery/jquery-3.2.1.min.js"></script>
+    <script src="../vendors/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="../vendors/owl-carousel/owl.carousel.min.js"></script>
+    <script src="../vendors/Magnific-Popup/jquery.magnific-popup.min.js"></script>
+    <script src="../vendors/nice-select/jquery.nice-select.min.js"></script>
+    <script src="../js/jquery.ajaxchimp.min.js"></script>
+    <script src="../js/mail-script.js"></script>
+    <script src="../js/skrollr.min.js"></script>
+    <!-- <script src="js/countdown.js"></script> -->
+    <script src="../js/jquery.magnific-popup.min.js"></script>
+    <script src="../js/main.js"></script>
+  </body>
+  </html>
