335c59fbee151defab592867f8c1ae24102b8a42
Verdy Bangkit <verdybangkit@gmail.com>
2019-06-10 17:45:47 +0700

    add place and category dropdown

--- App/Views/user/buat-post.php
+++ App/Views/user/buat-post.php
@@ -35,6 +35,14 @@
 <link rel="stylesheet" href="../vendors/flat-icon/font/flaticon.css">
 <link rel="stylesheet" href="../vendors/nice-select/nice-select.css">
 <link rel="stylesheet" href="../css/style.css">
+<script src="../vendors/jquery/jquery-3.2.1.min.js"></script>
+<script src="../vendors/bootstrap/bootstrap.bundle.min.js"></script>
+<script src="../vendors/owl-carousel/owl.carousel.min.js"></script>
+<script src="../vendors/nice-select/jquery.nice-select.min.js"></script>
+<script src="../js/jquery.ajaxchimp.min.js"></script>
+<script src="../js/mail-script.js"></script>
+<script src="../js/skrollr.min.js"></script>
+<script src="../js/main.js"></script>
 
 </head>
 <body class="bg-shape">
@@ -49,24 +57,40 @@
       <div class="row justify-content-center">
         <div class="col-lg-10">
           <form method="post">
-            <div class="col-12">
-              <div class="form-group">
-                <input class="form-control form-control-lg" type="text" name="title" id="title" placeholder="Title">
+            <div class="row">
+              <div class="col-12">
+                <div class="form-group">
+                  <input class="form-control form-control-lg" type="text" name="title" id="title" placeholder="Title">
+                </div>
               </div>
-            </div>
-            <div class="col-12">
-              <div class="form-group">
-                <input class="form-control" name="subject" id="subject" type="text" placeholder="Enter label">
+              <div class="col-12">
+                <div class="form-group">
+                  <div class="row">
+                    <div class="col-6">
+                      <div class="form-inline">
+                        <select class="col-12" id="category">
+                          <option value="disabled" disabled selected>Category</option>
+                          <option value="harem">Harem</option>
+                          <option value="hentai">Hentai</option>
+                          <option value="ecchi">Ecchi</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div class="col-6">
+                      <input class="form-control" type="text" name="tempat" id="tempat" placeholder="Tempat">
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="col-12">
-              <div class="form-group">
-                <textarea id="mceEditor" name="area" placeholder="input text here" style="height: 480px; resize: none;"></textarea>
+              <div class="col-12">
+                <div class="form-group">
+                  <textarea id="mceEditor" name="area" placeholder="input text here" style="height: 480px; resize: none;"></textarea>
+                </div>
               </div>
-            </div>
-            <div class="col-12">
-              <div class="form-group">
-                <button type="submit" class="button">POST</button>
+              <div class="col-12">
+                <div class="form-group">
+                  <button type="submit" class="button">POST</button>
+                </div>
               </div>
             </div>
           </form>
@@ -75,6 +99,5 @@
       </div>
     </div>
   </section>
-
 </body>
 </html>
